f900bb2836af6a6be66569b3c0ae57d1572866bd
Rafael Sindice <rsindice@apoioprodesp.sp.gov.br>
2023-03-14 16:31:33 -0300

    medico

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,7 +1,5 @@
 package med.voll.api.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
