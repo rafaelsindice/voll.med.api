69ca60df9588df88b26654a8d0c4d980094a7903
Rafael Sindice <rsindice@apoioprodesp.sp.gov.br>
2023-03-14 15:29:59 -0300

    listagem dos medicos e listagem de pacientes

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,7 +1,13 @@
 package med.voll.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 
@@ -21,5 +28,10 @@ public class MedicoController {
 @Transactional
 public void cadastrar(@RequestBody @Valid DadosCadastroMedico  dados) {
 	repository.save(new Medico(dados));
+}
+@GetMapping
+public Page<DadosListagemMedico>listar(@PageableDefault(size = 10,sort = {"nome"})Pageable paginacao){
+	return repository.findAll(paginacao).map(DadosListagemMedico::new);
+	
 }
 }
